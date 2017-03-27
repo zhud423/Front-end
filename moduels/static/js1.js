3a597806e87e878405736712e43606ba02f94280
zhud423 <zhud423@163.com>
2017-03-27 21:49:21 +0800

    update

--- moduels/static/js1.js
+++ moduels/static/js1.js
@@ -4,19 +4,20 @@
 // 所有模块都通过 define 来定义
 define(function(require, exports, module) {
 
-    // 通过 require 引入依赖
 
-    // 通过 exports 对外提供接口
-    //exports.d =function(){   //自定义方法
-    //    console.log("ok");
-    //};
-    //exports.d1 =function(){   //自定义方法
-    //    console.log("ok1");
-    //};
-    //exports.d2 =function(){   //自定义方法
-    //    console.log("ok2");
-    //};
+    // 通过 exports 分别对外提供接口
+    exports.d =function(){   //自定义方法
+        console.log("ok");
+    };
+    exports.d1 =function(){   //自定义方法
+        console.log("ok1");
+    };
+    exports.d2 =function(){   //自定义方法
+        console.log("ok2");
+    };
 
+
+    // 通过 module.exports 提供整个接口
     var a={};
     a.d =function(){
         console.log("ok");
@@ -27,8 +28,6 @@ define(function(require, exports, module) {
     a.d2 =function(){
         console.log("ok2");
     };
-    //exports.a=a;    此法也能实现,但几个方法包含在对象a里面
-    // 或者通过 module.exports 提供整个接口
     module.exports =a;
 
 });
